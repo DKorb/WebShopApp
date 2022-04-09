505d28d45c80397e99554c9c0806a93560ab587e
DKorb <dawid.korbecki@o2.pl>
2022-04-09 15:07:29 +0200

    Added Main controller and index.html

---
+++ src/main/java/WebShopApp/Application/Controller/MainController.java
@@ -0,0 +1,13 @@
+package WebShopApp.Application.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping
+    public String viewHomePage() {
+        return "index";
+    }
+}
