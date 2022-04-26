be7b986a066da8822b7353a860010ec9279a2f30
DKorb <dawid.korbecki@o2.pl>
2022-04-26 20:01:15 +0200

    Added new exception for user not found

---
+++ src/main/java/WebShopApp/Application/Exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package WebShopApp.Application.Exceptions;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
