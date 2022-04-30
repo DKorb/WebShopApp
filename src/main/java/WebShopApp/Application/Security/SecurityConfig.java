d966f16ea012e93a76291e5783f649b84c028d9d
DKorb <dawid.korbecki@o2.pl>
2022-04-30 11:26:03 +0200

    Added login page and authorization request in SecurityConfig

--- src/main/java/WebShopApp/Application/Security/SecurityConfig.java
+++ src/main/java/WebShopApp/Application/Security/SecurityConfig.java
@@ -3,6 +3,7 @@ package WebShopApp.Application.Security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,6 +20,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("email")
+                .permitAll();
     }
+
+/*    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
+    }*/
 }
