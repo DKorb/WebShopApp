3219817ab411736e184deec509aac2877292e1b3
DKorb <dawid.korbecki@o2.pl>
2022-05-23 10:39:01 +0200

    Added product entity and repository

---
+++ src/main/java/WebShopApp/Application/Entity/Product.java
@@ -0,0 +1,60 @@
+package WebShopApp.Application.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, length = 64, nullable = false)
+    private String name;
+
+    @Column(unique = true, length = 64, nullable = false)
+    private String alias;
+
+    @Column(name = "short_description", length = 512, nullable = false)
+    private String shortDescription;
+
+    @Column(name = "full_description", length = 4096, nullable = false)
+    private String fullDescription;
+
+    @Column(name = "created_time")
+    private Date createdTime;
+
+    @Column(name = "updated_time")
+    private Date updatedTime;
+
+    private boolean status;
+
+    @Column(name = "in_stock")
+    private boolean inStock;
+
+    private float cost;
+    private float price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/WebShopApp/Application/Repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package WebShopApp.Application.Repository;
+
+import WebShopApp.Application.Entity.Product;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+
+}
