d2f9ab1a70b202172ea70c87afb98663298716fa
DKorb <dawid.korbecki@o2.pl>
2022-05-27 17:30:59 +0200

    Added functionality of adding photo to product + new directory has been registered

--- src/main/java/WebShopApp/Application/Controller/File/ApplicationConfig.java
+++ src/main/java/WebShopApp/Application/Controller/File/ApplicationConfig.java
@@ -14,6 +14,7 @@ public class ApplicationConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         imagesDirectory("category-images", registry);
         imagesDirectory("brand-images", registry);
+        imagesDirectory("product-images", registry);
     }
 
     private void imagesDirectory(String pathPattern, ResourceHandlerRegistry registry) {
--- src/main/java/WebShopApp/Application/Controller/ProductController.java
+++ src/main/java/WebShopApp/Application/Controller/ProductController.java
@@ -1,5 +1,6 @@
 package WebShopApp.Application.Controller;
 
+import WebShopApp.Application.Controller.File.FileUploadUtil;
 import WebShopApp.Application.Entity.Brand;
 import WebShopApp.Application.Entity.Product;
 import WebShopApp.Application.Exceptions.ProductNotFoundException;
@@ -8,11 +9,15 @@ import WebShopApp.Application.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -49,7 +54,22 @@ public class ProductController {
     }
 
     @PostMapping("/products/save")
-    public String saveProduct(Product product, RedirectAttributes redirectAttributes) {
+    public String saveProduct(Product product, @RequestParam("fileImage") MultipartFile multipartFile,
+                              RedirectAttributes redirectAttributes) throws IOException {
+
+        if (!multipartFile.isEmpty()) {
+            String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+            product.setImage(fileName);
+
+            Product savedProduct = productService.save(product);
+            String uploadDir = "product-images/" + savedProduct.getId();
+
+            FileUploadUtil.cleanDirectory(uploadDir);
+            FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+        } else {
+            productService.save(product);
+        }
+
         productService.save(product);
         redirectAttributes.addFlashAttribute("message",
                 "The product has been saved successfully.");
