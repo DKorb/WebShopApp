45701e3f66af4e588835076d94699aa2d504eb81
DKorb <dawid.korbecki@o2.pl>
2022-05-28 14:33:32 +0200

    Added JS codes to remove html input in product details tab

---
+++ src/main/resources/static/javascript/product_form_details.js
@@ -0,0 +1,34 @@
+function addNextDetailSection() {
+    allDivDetails = $("[id^='divDetail']")
+    divDetailsCount = allDivDetails.length;
+
+    htmlDetailSection = `
+    <div class="form-inline" id="divDetail${divDetailsCount}">
+        <label class="m-3">
+            Name:
+            <input type="text" class="form-control" name="detailNames" maxlength="64"/>
+        </label>
+        <label class="m-3">
+            Value:
+            <input type="text" class="form-control" name="detailNames" maxlength="64"/>
+        </label>
+    </div>
+    `;
+
+    $("#productDetails").append(htmlDetailSection);
+
+    previousDivDetailSection = allDivDetails.last();
+    previousDivDetailID = previousDivDetailSection.attr("id");
+
+    htmlLinkRemove = `
+    <a class="far fa-minus-square" style="color: #2460c2" 
+       href="javascript:removeDetailSectionById('${previousDivDetailID}')"
+       title="remove this item"></a>
+    `;
+
+    previousDivDetailSection.append(htmlLinkRemove);
+}
+
+function removeDetailSectionById(id) {
+    $("#" + id).remove();
+}
