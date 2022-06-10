84af0028b0c2899f40039d920e66540b04dca56a
DKorb <dawid.korbecki@o2.pl>
2022-06-10 19:11:49 +0200

    Added unit test for query retrieving product by alias

---
+++ WebParent/CustomerPanel/src/test/java/com/webshopapp/customerpanel/ProductRepositoryTests.java
@@ -0,0 +1,26 @@
+package com.webshopapp.customerpanel;
+
+import com.webshopapp.common.entity.product.Product;
+import com.webshopapp.customerpanel.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class ProductRepositoryTests {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    public void getProductByAlias() {
+        String alias = "Lenovo_Flex_3i_Chromebook";
+        Product productByAlias = productRepository.getProductByAlias(alias);
+
+        assertThat(productByAlias).isNotNull();
+    }
+}
