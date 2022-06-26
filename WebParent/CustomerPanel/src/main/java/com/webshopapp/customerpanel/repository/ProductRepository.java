a311bc6ce32a91df722d27ae54a2bbe167c224c6
DKorb <dawid.korbecki@o2.pl>
2022-06-26 15:20:16 +0200

    Added query in product repo to search for products

--- WebParent/CustomerPanel/src/main/java/com/webshopapp/customerpanel/repository/ProductRepository.java
+++ WebParent/CustomerPanel/src/main/java/com/webshopapp/customerpanel/repository/ProductRepository.java
@@ -9,8 +9,13 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
 
     @Query("SELECT p FROM Product p WHERE p.category.id = ?1 AND p.status = true ORDER BY p.name ASC")
-    public Page<Product> getAllProductByCategory(Integer categoryID, Pageable pageable);
+    Page<Product> getAllProductByCategory(Integer categoryID, Pageable pageable);
 
     @Query("SELECT p FROM Product p WHERE p.alias = ?1")
-    public Product getProductByAlias(String alias);
+    Product getProductByAlias(String alias);
+
+    @Query(value = "SELECT * FROM products WHERE status = true AND " +
+            "MATCH(name, full_description) AGAINST (?1)", nativeQuery = true)
+    Page<Product> searchProductByNameAndFullDescription(String keyword, Pageable pageable);
+
 }
