02291ece5b4a87578918e99a217a0c34367ec2b6
DKorb <dawid.korbecki@o2.pl>
2022-08-07 13:16:57 +0200

    Added Cart entity + repo with methods for shopping cart

---
+++ Common/src/main/java/com/webshopapp/common/entity/cart/Cart.java
@@ -0,0 +1,34 @@
+package com.webshopapp.common.entity.cart;
+
+import com.webshopapp.common.entity.customer.Customer;
+import com.webshopapp.common.entity.product.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cart_items")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity;
+
+}
--- WebParent/CustomerPanel/src/main/java/com/webshopapp/customerpanel/category/CategoryRepository.java
+++ WebParent/CustomerPanel/src/main/java/com/webshopapp/customerpanel/category/CategoryRepository.java
@@ -9,8 +9,8 @@ import java.util.List;
 public interface CategoryRepository extends CrudRepository<Category, Integer> {
 
     @Query("SELECT c FROM Category c WHERE c.status = true ORDER BY c.name ASC")
-    public List<Category> findAllEnabledCategories();
+    List<Category> findAllEnabledCategories();
 
     @Query("SELECT c FROM Category c WHERE c.alias = ?1 AND c.status = true")
-    public Category findCategoryByAlias(String alias);
+    Category findCategoryByAlias(String alias);
 }
