da16a7400f1250b4db8da60ef3d93be8e6e33987
DKorb <dawid.korbecki@o2.pl>
2022-08-07 13:36:30 +0200

    Added JS to control number of items in cart + new thymeleaf view

---
+++ WebParent/CustomerPanel/src/main/resources/static/javascript/quantity_control.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $(".linkMinus").on("click", function(evt) {
+        evt.preventDefault();
+        productId = $(this).attr("pid");
+        quantityInput = $("#quantity" + productId);
+        newQuantity = parseInt(quantityInput.val()) - 1;
+
+        if (newQuantity > 0) {
+            quantityInput.val(newQuantity);
+        }
+
+    });
+
+    $(".linkPlus").on("click", function(evt) {
+        evt.preventDefault();
+        productId = $(this).attr("pid");
+        quantityInput = $("#quantity" + productId);
+        newQuantity = parseInt(quantityInput.val()) + 1;
+
+        if (newQuantity <= 10) {
+            quantityInput.val(newQuantity);
+        }
+
+    });
+});
